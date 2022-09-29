f0a96521bafae029756bcc37006aba9b6220d408
eternalme0w <lenny.study@yandex.ru>
2022-09-29 04:05:39 +0300

    negative nums and amount of elements of set

--- firstLab.cpp
+++ firstLab.cpp
@@ -8,9 +8,26 @@
 using std::cin, std::cout, std::endl;
 
 
+bool in_set(int* mass, int el) {
+
+	bool ans = 0; 
+
+	for (int i = 0; i < 10; i++) {
+
+		if (mass[i] == el) {
+
+			ans = 1;
+			break;
+		}
+	}
+
+	return ans;
+}
+
 int* get_mass() {
 
 	static int mass[10];
+	static int arr;
 
 	for (int i = 0; i < 10; mass[i++] = -1);
 
@@ -19,7 +36,7 @@ int* get_mass() {
 
 	cout << "Enter 10 or less numbers from 0 to 9: ";
 
-	while(cin.peek() != 10 || a > 9) {
+	while(cin.peek() != 10 || a > 9 || a <= 0) {
 
 		CLS;
 
@@ -32,7 +49,7 @@ int* get_mass() {
 
 		cout << "Enter 10 or less numbers from 0 to 9: ";
 
-		if (cin >> a &&  a < 10) mass[i++] = a;
+		if (cin >> a &&  a < 10 && a > 0 && !in_set(mass, a)) mass[i++] = a;	
 	}
 
 	CLS;
@@ -44,7 +61,12 @@ int* get_mass() {
 int massln(int* mass) {
 
 	int i;
-	for (i = 0; mass[i] != -1; i++);
+
+	for (i = 0; i < 10; i++) {
+
+		if (mass[i] == -1) break;
+	}
+
 	return i;
 }
 
@@ -59,7 +81,7 @@ int main() {
 
 	int* mass = get_mass();
 
-	print_mass(mass);
+	print_mass(mass);	
 
 	return 0;
 }
